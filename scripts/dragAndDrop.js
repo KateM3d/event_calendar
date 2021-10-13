2ea2b1b668694394143672309ca1d817128d4e7a
KateM3d <ekaterina8medvedeva@gmail.com>
2021-10-13 18:02:49 -0400

    temp commit

--- scripts/dragAndDrop.js
+++ scripts/dragAndDrop.js
@@ -1,40 +1,40 @@
 // function for the dragANdDrop part. Waiting for calendar and complete todo
 
-const dragAndDrop = () => {
-    const task = document.querySelector('.taskCard');
-    const dateCells = document.querySelector('.callendarCell');
+// const dragAndDrop = () => {
+//     const task = document.querySelector('#labelForItem');
+//     const dateCells = document.querySelector('.card-body');
 
-    const dragStart = () => {
-        setTimeout(() => {
-            this.classList.add('hide');
-        }, 0);
-    };
-    const dragEnd = () => {
-        this.classList.remove('hide');
-    };
+//     const dragStart = () => {
+//         setTimeout(() => {
+//             this.classList.add('hide');
+//         }, 0);
+//     };
+//     const dragEnd = () => {
+//         this.classList.remove('hide');
+//     };
 
-    const dragOver = (e) => {
-        e.preventDefault();
-    };
-    const dragEnter = () => {
-        setTimeout(() => {
-            this.classList.add('hovered');
-        }, 0);
-    };
-    const dragLeave = () => {
-        this.classList.remove('hovered');
-    };
-    const dragDrop = () => {
-        this.append(task);
-        this.classList.remove('hovered');
+//     const dragOver = (e) => {
+//         e.preventDefault();
+//     };
+//     const dragEnter = () => {
+//         setTimeout(() => {
+//             this.classList.add('hovered');
+//         }, 0);
+//     };
+//     const dragLeave = () => {
+//         this.classList.remove('hovered');
+//     };
+//     const dragDrop = () => {
+//         this.append(task);
+//         this.classList.remove('hovered');
 
-    };
+//     };
 
-    dateCells.forEach((cell) => {
-        cell.addEventListener('dragover', dragOver);
-        cell.addEventListener('dragenter', dragEnter);
-        cell.addEventListener('dragleave', dragLeave);
-        cell.addEventListener('drop', dragDrop);
+//     dateCells.forEach((cell) => {
+//         cell.addEventListener('dragover', dragOver);
+//         cell.addEventListener('dragenter', dragEnter);
+//         cell.addEventListener('dragleave', dragLeave);
+//         cell.addEventListener('drop', dragDrop);
 
-    })
-}
+//     })
+// }
--- scripts/scriptToDo.js
+++ scripts/scriptToDo.js
@@ -1,16 +1,18 @@
 let addMessage = document.querySelector('#in');
 let addButton = document.querySelector('#add');
-let todo=document.querySelector('#out');
+let todo = document.querySelector('#out');
 
-let todoList=[];
+let todoList = [];
 
-if(localStorage.getItem('todo')){
-    todoList=JSON.parse(localStorage.getItem('todo'));
+if (localStorage.getItem('todo')) {
+    todoList = JSON.parse(localStorage.getItem('todo'));
     displayMessages();
 }
 
-addButton.addEventListener('click', function () {
-    if(!addMessage.value)return;
+
+
+addButton.addEventListener('click', function() {
+    if (!addMessage.value) return;
 
     let newTodo = {
         todo: addMessage.value,
@@ -19,43 +21,91 @@ addButton.addEventListener('click', function () {
 
     todoList.push(newTodo);
     displayMessages();
-    localStorage.setItem('todo',JSON.stringify(todoList));
-    addMessage.value='';
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    addMessage.value = '';
+
 });
 
-function displayMessages(){
-    let displayMessages='';
-if(todoList.length===0)todo.innerHTML='';
-    todoList.forEach(function(item, i){
-displayMessages += `
+function displayMessages() {
+    let displayMessages = '';
+    if (todoList.length === 0) todo.innerHTML = '';
+    todoList.forEach(function(item, i) {
+        displayMessages += `
 <li>
-<div id="lebelForItem"  class="${item.important ? 'important': ''}" class="bg-secondary text-white"  ondblclick="deleteTask(${i})">${item.todo}</div>
+<div id="labelForItem"  class="${item.important ? 'important': ''}" class="bg-secondary text-white"  ondblclick="deleteTask(${i})">${item.todo}</div>
 </li>
 `;
-todo.innerHTML=displayMessages;
+        todo.innerHTML = displayMessages;
     });
+
 }
 
-function deleteTask(i){
+function deleteTask(i) {
 
-        todoList=JSON.parse(localStorage.getItem('todo'));
-        todoList.splice(i,1);
-        localStorage.setItem('todo',JSON.stringify(todoList));
-        displayMessages();
+    todoList = JSON.parse(localStorage.getItem('todo'));
+    todoList.splice(i, 1);
+    localStorage.setItem('todo', JSON.stringify(todoList));
+    displayMessages();
+};
+
+
+todo.addEventListener('click', function(event) {
+    event.preventDefault();
+    todoList.forEach(function(item, i) {
+        if (item.todo === event.target.innerHTML) {
+            if (event.ctrlKey || event.metaKey) {
+                todoList.splice(i, 1);
+            } else {
+                item.important = !item.important;
+            }
+            displayMessages();
+            localStorage.setItem('todo', JSON.stringify(todoList));
+        }
+    });
+});
+
+
+
+const dragAndDrop = function() {
+    const task = document.querySelector('#labelForItem');
+    const dateCells = document.querySelector('.card-body');
+
+    const dragStart = function() {
+        setTimeout(() => {
+            this.classList.add('hide');
+        }, 0);
+    };
+    const dragEnd = function() {
+        this.classList.remove('hide');
     };
 
+    const dragOver = function(e) {
+        e.preventDefault();
+    };
+    const dragEnter = function() {
+        setTimeout(() => {
+            this.classList.add('hovered');
+        }, 0);
+    };
+    const dragLeave = function() {
+        this.classList.remove('hovered');
+    };
+    const dragDrop = function() {
+        this.append(task);
+        this.classList.remove('hovered');
 
-todo.addEventListener('click',function(event){
-    event.preventDefault();
-todoList.forEach(function(item,i){
-    if(item.todo===event.target.innerHTML){
-        if(event.ctrlKey || event.metaKey){
-            todoList.splice(i,1);
-        } else{
-            item.important=!item.important;
-        }        
-        displayMessages();
-        localStorage.setItem('todo',JSON.stringify(todoList));
-    }
-});
-});
+    };
+
+    dateCells.forEach((cell) => {
+        cell.addEventListener('dragover', dragOver);
+        cell.addEventListener('dragenter', dragEnter);
+        cell.addEventListener('dragleave', dragLeave);
+        cell.addEventListener('drop', dragDrop);
+
+    })
+
+    task.addEventListener('dragstart', dragStart);
+    task.addEventListener('dragend', dragEnd);
+}
+
+dragAndDrop()
